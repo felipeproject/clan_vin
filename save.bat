7f99ed0fd3c303d17430ebd588e5993e3c1f56ef
felipeproject <feelipe_andree@hotmail.com>
2024-11-02 08:25:58 -0300

    Atualiza├º├╡es feitas

---
+++ save.bat
@@ -0,0 +1,16 @@
+@echo off
+
+:: Adicione as alterações ao staging
+git add .
+
+:: Faça um commit com uma mensagem padrão
+git commit -m "Atualizações feitas"
+
+:: Envie as alterações para o repositório remoto
+git push origin main
+
+:: Para repositórios em outras branches, substitua "main" pelo nome da sua branch
+:: git push origin sua-branch
+
+echo Atualização concluída!
+pause
